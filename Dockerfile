1344360411907a916a527eaec5e870fdfde6dec3
Seohyun Lee <studioSeoRii@gmail.com>
2022-08-27 16:10:55 +0900

    feat: add test server

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM python:3.9
+
+RUN curl -sL https://deb.nodesource.com/setup_18.x | bash
+RUN apt-get install nodejs -y
+RUN node --version
+RUN npm --version
+RUN npm install -g pnpm typescript@4.6.3
+RUN pnpm --version
+
+RUN npm install -g pnpm
+RUN pnpm --version
+
+# Init project
+COPY .gitignore package.json pnpm-lock.yaml tsconfig.json /KP/
+WORKDIR /KP
+RUN pnpm install
+COPY neopjuki/ /KP/neopjuki/
+WORKDIR /KP/neopjuki/
+RUN python3 --version
+RUN pip3 install wheel
+RUN pip3 install -r requirements.txt
+COPY build/index.js /KP/build/index.js
+
+# Run server
+ENTRYPOINT ["node", "/KP/build/index.js"]
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,34 @@
 import App from '@hancomeducation/bedrock'
+import {spawn} from 'child_process'
 
 declare const config: { version: string, commitHash: string, commitCount: number, buildDate: string };
 
 App({
-    config, port: 2000, name: 'kaporido', cb: async ({r}) => {
-
+    config, port: 5500, name: 'kaporido', cb: async ({ws}) => {
+        ws('/game', (ws, req) => {
+            const process = spawn('python3', ['./neopjuki/src/mcts.py', '--trials=1', '--workers=4', '--depth=1', '--io=True', '--p2e=True'], {})
+            ws.onmessage = (e) => {
+                process.stdin.write(e.data.toString() + '\n')
+            }
+            process.stdout.on('data', (data) => {
+                data = data.toString().split('\n')
+                for(const line of data) {
+                    console.log(line)
+                    if (line[0] !== '0' && line[0] !== '1' && line[0] !== '2' && line[0] !== '3') continue;
+                    ws.send(line)
+                }
+            })
+            process.stderr.on('data', (data) => {
+                console.log(data.toString())
+            })
+            process.on('close', (code) => {
+                console.log(code)
+                ws.close()
+            })
+            ws.close = () => {
+                process.kill()
+            }
+        })
     }
 }).then()
 
